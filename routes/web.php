6dc3c97c7c432061ef777b4bee511cdd2bef3ae4
WilsonValenciaCepeda <ricardostia7u7@gmail.com>
2026-06-26 19:55:07 -0400

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,6 @@ use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 // Página de inicio
 Route::get('/', function () {
     return view('inicio');
